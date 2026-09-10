f691726b73a0c4ffe7f626e4829fdb4a930b9313
gogodedebam <gogodedebam@gmail.com>
2026-09-10 15:15:11 -0400

    pagina nosotros

---
+++ resources/views/nosotros.blade.php
@@ -0,0 +1,23 @@
+<x-layout>
+
+    <section class="mx-auto max-w-4xl px-6 py-16">
+
+        <div class="rounded-2xl bg-white p-8 shadow-lg md:p-12">
+
+            <h1 class="text-4xl font-bold">
+                Nosotros
+            </h1>
+
+            <p class="mt-6 text-lg leading-relaxed text-slate-600">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae aliquid, nesciunt pariatur provident voluptate incidunt error esse ducimus distinctio exercitationem voluptatem doloribus, cum fugit facere commodi. Natus labore non id!
+            </p>
+
+            <p class="mt-4 leading-relaxed text-slate-600">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio sed laudantium ad quod architecto voluptatem esse nisi eligendi culpa incidunt! Doloribus facere adipisci magni debitis reiciendis sit sunt veniam autem.
+            </p>
+
+        </div>
+
+    </section>
+
+</x-layout>
